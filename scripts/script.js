e14f6ef58289967f3b094bab8e73245949c34dc3
Yesterday's Lunchbox <yesterdayslunchbox@gmail.com>
2025-08-22 11:57:25 -0500

    Added footers to pages

--- scripts/script.js
+++ scripts/script.js
@@ -29,3 +29,9 @@ Promise.all(
     postContainer.appendChild(div);
   });
 }).catch(err => console.error('Failed to load posts:', err));
+
+// Function to set the current year in the footer
+document.addEventListener('DOMContentLoaded', function() {
+  const currentYear = new Date().getFullYear();
+  document.getElementById(`currentYearPlaceholder`).textContent = currentYear;
+});
